5b8a10f8fb2c7c71907e7b2a7d711d61d2d51f49
Martin Geuer <martin.geuer@erdkante.de>
2015-10-14 09:14:00 +0200

    created small source writer

---
+++ src/main/java/org/rapla/gwtjsonrpc/annotation/SourceWriter.java
@@ -0,0 +1,58 @@
+package org.rapla.gwtjsonrpc.annotation;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public class SourceWriter
+{
+    private final PrintWriter printWriter;
+    private int indent = 0;
+    private boolean newLine = true;
+
+    public SourceWriter(Writer writer)
+    {
+        printWriter = new PrintWriter(writer);
+    }
+
+    public SourceWriter(OutputStream os)
+    {
+        printWriter = new PrintWriter(os);
+    }
+
+    public void indent()
+    {
+        indent++;
+    }
+
+    public void outdent()
+    {
+        indent--;
+    }
+
+    public void println(String content)
+    {
+        writeIndent();
+        printWriter.println(content);
+        newLine = true;
+    }
+
+    private void writeIndent()
+    {
+        if (newLine)
+        {
+            for (int i = 0; i < indent; i++)
+            {
+                printWriter.print("  ");
+            }
+        }
+    }
+
+    public void print(String content)
+    {
+        writeIndent();
+        printWriter.print(content);
+        newLine = false;
+    }
+
+}
