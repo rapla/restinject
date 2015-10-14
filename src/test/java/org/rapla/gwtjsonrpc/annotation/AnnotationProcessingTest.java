e48300fb0e39b2890c1f51c038a9bad0b9944d2b
Martin Geuer <martin.geuer@erdkante.de>
2015-10-14 09:13:49 +0200

    extended test

--- src/test/java/org/rapla/gwtjsonrpc/annotation/AnnotationProcessingTest.java
+++ src/test/java/org/rapla/gwtjsonrpc/annotation/AnnotationProcessingTest.java
@@ -1,13 +1,28 @@
 package org.rapla.gwtjsonrpc.annotation;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 import org.rapla.gwtjsonrpc.RemoteJsonMethod;
 import org.rapla.gwtjsonrpc.common.FutureResult;
 import org.rapla.gwtjsonrpc.common.ResultType;
-
-import java.util.Date;
 
 @RemoteJsonMethod
 public interface AnnotationProcessingTest
 {
     @ResultType(String.class)
-    FutureResult<String> sayHello(String message);
-}
+    FutureResult<Result> sayHello(Parameter param);
+    
+    public static class Result {
+        private String name;
+        private List<String> ids;
+        
+    }
+    
+    public static class Parameter
+    {
+        private Map<String, List<String>> requestedIds;
+        private List<Integer> actionIds;
+        private Date lastRequestTime;
+    }
+}   
